a7a30adb41b90c2833db5f2bbcd9e27d08864553
Harshil Patel <h.gibbs62@gmail.com>
2018-02-12 17:06:03 -0600

    Video player fix and update content

--- js/agency.js
+++ js/agency.js
@@ -47,4 +47,39 @@
     $(".navbar").removeClass("d-none");
   })
 
+  var src = $('.video-container').children('iframe').attr('src');
+  
+  $('.open-popup').click(function(e) {
+    e.preventDefault();
+    // change the src value of the video
+    $('.video-container').children('iframe').attr('src', src);
+  });
+
+  // when object with class close-popup is clicked...
+  $('.close-popup').click(function(e) {
+    e.preventDefault();
+    $('.video-container').children('iframe').attr('src', '');
+  });
+
 })(jQuery); // End of use strict
+
+
+$(document).ready(function() {
+  // set unique id to videoplayer for the Webflow video element
+  var src = $('#videoplayer').children('iframe').attr('src');
+
+  // when object with class open-popup is clicked...
+  $('.open-popup').click(function(e) {
+    e.preventDefault();
+    // change the src value of the video
+    $('#videoplayer').children('iframe').attr('src', src);
+    $('.popup-bg').fadeIn();
+  });
+
+  // when object with class close-popup is clicked...
+  $('.close-popup').click(function(e) {
+    e.preventDefault();
+    $('#videoplayer').children('iframe').attr('src', '');
+    $('.popup-bg').fadeOut();
+  });
+});

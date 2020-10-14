e84e85934a50ce2749dba5ff59c85e549043ae30
Harshil Patel <h.gibbs62@gmail.com>
2020-10-13 20:03:30 -0500

    Certs spacing and contact form update

--- js/contact_me.js
+++ js/contact_me.js
@@ -20,7 +20,7 @@ $(function() {
       $this = $("#sendMessageButton");
       $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
       $.ajax({
-        url: "//formspree.io/h.gibbs62@gmail.com",
+        url: "//formspree.io/harshildpatel@outlook.com",
         type: "POST",
         data: {
           name: name,
